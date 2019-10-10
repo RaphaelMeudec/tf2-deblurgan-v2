c99d9cfcf29df4e38bf171d13c6f0645af193298
Raphael Meudec <raphaelm@sicara.com>
2019-10-10 14:11:03 +0200

    Add discriminator, create Trainer class

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 from dataset import load_dataset
-from model import load_generator
+from model import load_discriminator, load_generator
 
 print(tf.__version__)
 
@@ -9,34 +9,49 @@ BATCH_SIZE = 4
 PATCH_SIZE = (256, 256)
 INPUT_SHAPE = (*PATCH_SIZE, 3)
 
-generator = load_generator(INPUT_SHAPE)
-generator(tf.zeros((1, *INPUT_SHAPE)))
-generator.summary()
 
-optimizer = tf.keras.optimizers.Adam(1e-4)
+class Trainer:
+    def __init__(self, dataset, input_shape):
+        self.dataset = dataset
+        self.input_shape = input_shape
 
-dataset = load_dataset("gopro", patch_size=PATCH_SIZE, batch_size=BATCH_SIZE, mode="train")
+        self.generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+        self.discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+
+        self.generator = load_generator(input_shape)
+        self.generator(tf.zeros((1, *input_shape)))
+        self.generator.summary()
+        self.discriminator = load_discriminator(input_shape)
+        self.discriminator(tf.zeros((1, *input_shape)))
+        self.discriminator.summary()
+
+    def train(self, num_batch):
+        for index, (sharp_images, blur_images) in enumerate(dataset):
+            if index > num_batch:
+                break
+
+            loss = self.training_loop(sharp_images, blur_images)
+            print(loss)
+
+    @tf.function
+    def training_loop(self, sharp_images, blur_images):
+        with tf.GradientTape() as tape:
+            blurred = tf.cast(blur_images, tf.float32)
+            sharp = tf.cast(sharp_images, tf.float32)
+
+            deblurred = self.generator(blurred, training=True)
+
+            loss = tf.reduce_sum(tf.abs(deblurred - sharp))
+
+        grads = tape.gradient(loss, self.generator.trainable_weights)
+
+        self.generator_optimizer.apply_gradients(zip(grads, self.generator.trainable_weights))
+
+        return loss
 
 
-@tf.function
-def training_loop(model, sharp_images, blur_images):
-    with tf.GradientTape() as tape:
-        blurred = tf.cast(blur_images, tf.float32)
-        sharp = tf.cast(sharp_images, tf.float32)
+if __name__ == "__main__":
+    dataset = load_dataset("gopro", patch_size=PATCH_SIZE, batch_size=BATCH_SIZE, mode="train")
 
-        deblurred = model(blurred, training=True)
-
-        loss = tf.reduce_sum(tf.abs(deblurred - sharp))
-
-    grads = tape.gradient(loss, model.trainable_weights)
-
-    optimizer.apply_gradients(zip(grads, model.trainable_weights))
-
-    return loss
-
-
-for index, (sharp_images, blur_images) in enumerate(dataset):
-    print(training_loop(generator, sharp_images, blur_images))
-
-    if index > 3:
-        break
+    trainer = Trainer(dataset, INPUT_SHAPE)
+    trainer.train(3)
--- model.py
+++ model.py
@@ -4,5 +4,17 @@ import tensorflow as tf
 def load_generator(input_shape):
     return tf.keras.Sequential([
         tf.keras.layers.Conv2D(16, 3, padding="same", activation='relu', input_shape=input_shape),
-        tf.keras.layers.Conv2D(3, 3, padding="same", activation='relu', input_shape=input_shape)
+        tf.keras.layers.Conv2D(3, 3, padding="same", activation='relu')
+    ])
+
+def load_discriminator(input_shape):
+    return tf.keras.Sequential([
+        tf.keras.layers.Conv2D(16, 3, padding="same", activation="relu", input_shape=input_shape),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Conv2D(16, 3, padding="same", activation="relu"),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Conv2D(16, 3, padding="same", activation="relu"),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(2, activation="softmax")
     ])
